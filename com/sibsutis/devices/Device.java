a98ee79326c617f2fd8850bdcb2329a939a0d543
Iren Ava <kenshin.kurieita@gmail.com>
2025-10-31 21:45:35 +0700

    Реализован класс Device

--- com/sibsutis/devices/Device.java
+++ com/sibsutis/devices/Device.java
@@ -3,46 +3,27 @@ package com.sibsutis.devices;
 import com.sibsutis.Printable;
 
 public abstract class Device implements Printable {
-    protected int device;
-    protected int price;
-    protected String ip;
+    private int id;
+    private int price;
+    private String ip;
 
     public Device(int id, int price, String ip) {
-        this.device = id;
-        this.price = price;
-        this.ip = ip;
+
     }
 
-    public int split() {
-        return device;
+    public int getID(){
+        return 0;
     }
 
-    public abstract String getDeviceType();
-
-    @Override
-    public String sprint() {
-        return String.format("Device{id=%d, price=%d, ip=%s, type=%s}", 
-                           device, price, ip, getDeviceType());
+    public int getPrice(){
+        return 0;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Device other = (Device) obj;
-        return device == other.device && price == other.price;
+    public String getIP(){
+        return "";
     }
 
-    @Override
-    public int hashCode() {
-        int result = 17;
-        result = 31 * result + device;
-        result = 31 * result + price;
-        result = 31 * result + (ip != null ? ip.hashCode() : 0);
-        return result;
+    public String getDeviceType(){
+        return "";
     }
-
-    public int getDevice() { return device; }
-    public int getPrice() { return price; }
-    public String getIp() { return ip; }
 }
