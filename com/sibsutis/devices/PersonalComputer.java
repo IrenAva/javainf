264a6cbae8f0e3a09d2ecfa49636583de4cff33f
IrenAva <156667007+IrenAva@users.noreply.github.com>
2025-11-01 10:44:11 +0700

    Добавлены методы equals и hashcode

--- com/sibsutis/devices/PersonalComputer.java
+++ com/sibsutis/devices/PersonalComputer.java
@@ -16,4 +16,20 @@ public class PersonalComputer extends Device {
     public String print() {
         return "";
     }
+    public boolean equals(PersonalComputer instance)
+    {
+        if (this.getID() == instance.getID() && 
+            this.getPrice() == instance.getPrice() &&
+            this.getIP() == instance.getIP())
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public int hashCode()
+    {
+        return this.getID() + this.getPrice() + (this.getIP().hashCode() * 31);
+    }
 }
